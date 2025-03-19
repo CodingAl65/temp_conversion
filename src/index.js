185b39f09e51e74254799f0e45f99d5cec66088b
chukwu albert <chuksalbert65@gmail.com>
2025-03-19 21:09:54 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+    // function to convert Farenheit to Celcius
+    function convertCtoF() {
+        let celsius = document.getElementById("textCelsius").value;
+        let fahrenheit = (celsius * 1.8) + 32;
+        document.getElementById("textFahrenheit").value = fahrenheit;
+    }
+
+
